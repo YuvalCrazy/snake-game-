dbd8039febbca11ef6f4d208f369f90ba8c12f12
yuval <yuval15407@gmail.com>
2025-01-15 16:19:14 +0200

    butons

--- app/src/main/java/com/example/snakegame/MainActivity.java
+++ app/src/main/java/com/example/snakegame/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.snakegame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+    Button login= findViewById(R.id.buttonlog);
+    Button signin = findViewById(R.id.buttonsign);
+    EditText password = findViewById(R.id.editTextTextPassword);
+    EditText email = findViewById(R.id.editTextTextEmailAddress);
+
+
+
 }
