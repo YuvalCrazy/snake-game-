7c5fc619af4cc477c8b8987bb0383bc4cae19ac3
yuval <yuval15407@gmail.com>
2025-01-22 16:19:47 +0200

    on create

--- app/src/main/java/com/example/snakegame/MainActivity.java
+++ app/src/main/java/com/example/snakegame/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.snakegame;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -7,19 +8,51 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAuth = FirebaseAuth.getInstance();
 
+    signin.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String emailsing = email.getText().toString().trim();
+            String passwordsing= password.getText().toString().trim();
+
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        }
+                    }
+
+        }
+    });
+
+    login.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+        }
+    });
     }
+
     Button login= findViewById(R.id.buttonlog);
     Button signin = findViewById(R.id.buttonsign);
     EditText password = findViewById(R.id.editTextTextPassword);
     EditText email = findViewById(R.id.editTextTextEmailAddress);
+
 
 
 
